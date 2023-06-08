3c970992e670591baff638fbedb38377ca1ea6ef
gangadhar20 <ganga2010@gmail.com>
2023-06-08 21:17:00 +0530

    satrun-file-added

---
+++ satrun.py
@@ -0,0 +1,19 @@
+cd /f/Bash_Scripts/
+ls
+vim remotexecmulti.sh
+vim remotexecmulti.sh
+vim MultiComRemExec.sh
+clear
+ls
+cp remotexecmulti.sh remotexecmulti.sh_13123030_1040.bakup
+ls
+vim remotexecmulti.sh
+cp remotexecmulti.sh_13123030_1040.bakup remotexecmulti.sh
+vim remotexecmulti.sh
+clear
+ls
+cp remotexecmulti.sh_14123030_1040.bakup remotexecmulti.sh
+cp remotexecmulti.sh remotexecmulti.sh_14123030_1040.bakup
+cp remotexecmulti.sh remotexecmulti.sh_12113030_1040.bakup
+cp remotexecmulti.sh remotexecmulti.sh_12103030_1150.bakup
+cp remotexecmulti.sh remotexecmulti.sh_05103030_1150.bakup
