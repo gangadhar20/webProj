94d78bf80c6a59193f08726b88a581770b2c466c
gangadhar20 <ganga2010@gmail.com>
2023-06-08 21:20:38 +0530

    few line removed

--- satrun.py
+++ satrun.py
@@ -17,8 +17,11 @@ cp remotexecmulti.sh_13123030_1040.bakup remotexecmulti.sh
 vim remotexecmulti.sh
 clear
 ls
+
+#########################
+### Removed few lines ###
+#########################
+
 cp remotexecmulti.sh_14123030_1040.bakup remotexecmulti.sh
 cp remotexecmulti.sh remotexecmulti.sh_14123030_1040.bakup
 cp remotexecmulti.sh remotexecmulti.sh_12113030_1040.bakup
-cp remotexecmulti.sh remotexecmulti.sh_12103030_1150.bakup
-cp remotexecmulti.sh remotexecmulti.sh_05103030_1150.bakup
