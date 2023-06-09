537fa326205da1964bb81eed54066c1ddc2e286b
gangadhar20 <ganga2010@gmail.com>
2023-06-09 19:55:30 +0530

    new file added on sub-branch

---
+++ Newsatrun.py
@@ -0,0 +1,27 @@
+cd /f/Bash_Scripts/
+ls
+vim remotexecmulti.sh
+vim remotexecmulti.sh
+vim MultiComRemExec.sh
+clear
+ls
+
+######################
+### New File Added ###
+######################
+
+cp remotexecmulti.sh remotexecmulti.sh_13123030_1040.bakup
+ls
+vim remotexecmulti.sh
+cp remotexecmulti.sh_13123030_1040.bakup remotexecmulti.sh
+vim remotexecmulti.sh
+clear
+ls
+
+#########################
+### Removed few lines ###
+#########################
+
+cp remotexecmulti.sh_14123030_1040.bakup remotexecmulti.sh
+cp remotexecmulti.sh remotexecmulti.sh_14123030_1040.bakup
+cp remotexecmulti.sh remotexecmulti.sh_12113030_1040.bakup
